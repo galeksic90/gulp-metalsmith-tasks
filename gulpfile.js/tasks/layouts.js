39130deb108f484b25d633f5fa5434550e21f324
Dr. Sinisa <sinisa.dragojlov@gmx.us>
2016-06-14 17:44:06 +0200

    normalize error output

--- gulpfile.js/tasks/layouts.js
+++ gulpfile.js/tasks/layouts.js
@@ -14,7 +14,8 @@ gulp.task('layouts:copy', function() {
 
 var msFunc = function(cb) {
     metalsmith_task.build(path.join(gulp.config.projectDir, gulp.config.roots.build), gulp.config.layouts.metalsmith, function (err, result) {
-        console.log('MS', err, result);
+        if (err)
+            console.log(err);
         cb();
     });
 };
