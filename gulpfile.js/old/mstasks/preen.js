2edf5ddd0a98c8761a6dc9b98752f7d25697af2e
Dr. Sinisa <sinisa.dragojlov@gmx.us>
2016-07-25 11:44:07 +0200

    remove unused stuff

--- gulpfile.js/old/mstasks/preen.js
+++
@@ -1,7 +0,0 @@
-var gulp = require('gulp');
-
-// Bower unused files remove - preen
-gulp.task('preen', function(cb) {
-	var preen = require('preen');
-	preen.preen({}, cb);
-});
--- gulpfile.js/old/oldindex.js
+++
@@ -1,7 +0,0 @@
-// ==== GULPFILE ==== //
-
-// This configuration follows the modular design of the `gulp-starter` project by Dan Tello: https://github.com/greypants/gulp-starter
-// Explore `tasks` for more...
-
-var requireDir = require('require-dir');
-requireDir('./tasks');
