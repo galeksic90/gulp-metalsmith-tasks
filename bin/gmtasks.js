44696f17db503c5e19c5b934b1a488ebd35888d6
Goran Aleksić <galeksic90@gmail.com>
2016-12-20 19:19:30 +0100

    gmtasks script fix

--- bin/gmtasks.js
+++ bin/gmtasks.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-process.stdout.write("gulp-metalsmith-tasks v1.0\r");
+process.stdout.write("gunmetal v1.0.1\r");
 process.env.GULP_PROJECTS_DIR = process.cwd();
-process.chdir(require('gulp-metalsmith-tasks').dirname);
+process.chdir(require('gunmetal').dirname);
 require('gulp-cli')();
